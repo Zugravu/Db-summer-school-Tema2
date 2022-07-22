b37bf35bad477798a40f3613cc4d621187fa6588
Zugravu Alexandru Stefan <alexandruzugra@gmail.com>
2022-07-22 13:06:05 +0300

    added 2 semaphores

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ import java.util.concurrent.Semaphore;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Semaphore semaphore = new Semaphore(1);
+        Semaphore semaphoreNS = new Semaphore(1);
+        Semaphore semaphoreWE = new Semaphore(1);
         Integer carsNS = 7; // Number of cars that have to pass the intersection from North to South
         Integer carsWE = 10; // Number of cars that have to pass the intersection from West to East
-        Road roadNS = new Road(semaphore,"NS", carsNS); // Created the North-South road
-        Road roadWE = new Road(semaphore,"WE", carsWE); // Created the West-East road
+        Road roadNS = new Road(semaphoreNS,"NS", carsNS); // Created the North-South road
+        Road roadWE = new Road(semaphoreWE,"WE", carsWE); // Created the West-East road
 
         roadNS.crossRoad();
         roadNS.join();
